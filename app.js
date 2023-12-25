1bdfdd0eb35b9cbafa6cc88d17c2032421e91f08
ariunbolor8989 <abojob8978@gmail.com>
2023-12-25 19:22:08 +0000

    hold towch ajiluulah

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //тоглох ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 //тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 25];
 // идэюртэй тоглогчын цуглуулсан оноог хадгалах функц
 var roundScore = 0;
 //Шоо аль талаараа буусныг хадгалах хувьсагч 1-6гэсэн утгыг энэ хувьсагчид санамсаргүйгээр оноож өгнө
-var dice = Math.floor(Math.random() * 6) + 1;
 
 window.document.getElementById("score-0").textContent = "0";
 window.document.getElementById("score-1").textContent = "0";
@@ -19,8 +18,41 @@ diceDom.style.display = "none";
 //diceDom.style.left="50%";
 //console.log(dice);
 
+//shoo shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceDom.style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // if (activePlayer === 0) {
+    //   document.querySelector(".player-1-panel").classList.remove("active");
+    //   document.querySelector(".player-0-panel").classList.add("active");
+    // } else {
+    //   document.querySelector(".player-0-panel").classList.remove("active");
+    //   document.querySelector(".player-1-panel").classList.add("active");
+    // }
+
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  diceDom.style.display = "none";
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".player-0-panel").classList.toggle("active");
 });
