7b4cefb8b2db5a11312fa53d3d019baec6d0ef47
ariunbolor8989 <abojob8978@gmail.com>
2023-12-25 17:55:03 +0000

    shoo shiddeg bolgokh

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ var roundScore = 0;
 //Шоо аль талаараа буусныг хадгалах хувьсагч 1-6гэсэн утгыг энэ хувьсагчид санамсаргүйгээр оноож өгнө
 var dice = Math.floor(Math.random() * 6) + 1;
 
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
 
 // window.document.querySelector("#score-1").innerHTML="<em>Yes!</em>";
-
-//document.querySelector(".dice").style.display="none";
-//document.querySelector(".dice").style.left="50%";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+//diceDom.style.left="50%";
 //console.log(dice);
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
